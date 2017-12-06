783cebcc029f86c07ba70c62989d2dfa6f4c1730
Izabely Furtado <izabely_furtado@hotmail.com>
2017-12-06 11:18:21 -0200

    Create TPA2GS
    para funcionar o graph stream
    importar biblioteca
    colocar na pasta algoGrafo;

---
+++ src/abstractGrafo/TPA2GS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prova;
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+import abstractGrafo.*;
+import abstractGrafo.Edge;
+
+/**
+ *
+ * @author 20121bsi0040
+ */
+public class TPA2GS {
+  private static String styleSheet = "";
+  
+  public sttaic void exibeGrafo(GrafoTPA g){
+    Graph graph = new SingleGraph("Tutorial 1");
+    System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+    LinkedList<Vertice> lst_Vs = g.vertices();
+    
+    //alimentando o grafo
+    for (int i = 0; i < lst_vs.size(); i++){
+      Node objGSNode = graph.addNode(lst_Vs.get(i).getLabel());
+      objGSNode.addAtribute("ui.label", lst_Vs.get(i).getLabel());
+    }
+    
+    graph.addAll("ui.stylesheet", styleSheet);
+    graph.setAutoCreate(true);
+    graph.setStrict(false);
+    graph.display();
+  }//exibeGarfo
+}
